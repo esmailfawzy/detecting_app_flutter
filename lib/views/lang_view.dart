18f2b32f6cb09f8fa04fe5f6f6c697672b12d985
Esmail Fawzy <esmail.elshishtawy.2001@gmail.com>
2024-05-12 09:13:56 +0300

    add tts in lang

--- lib/views/lang_view.dart
+++ lib/views/lang_view.dart
@@ -5,6 +5,10 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 class LangView extends StatelessWidget {
+  // final ttsController = TTSController();
+
+  // CustomButton({Key key, this.buttonType}) : super(key: key);
+
   const LangView({super.key});
 
   @override
@@ -13,37 +17,42 @@ class LangView extends StatelessWidget {
         appBar: AppBar(
           title: Text('Choose a language'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const CameraView(
-                              lang: "ar",
-                            )),
-                  );
-                },
-                child: Text('Arabic'),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const CameraView(
-                              lang: "en-US",
-                            )),
-                  );
-                },
-                child: Text('English'),
-              ),
-            ],
-          ),
-        ));
+        body: GetBuilder<TTSController>(
+            init: TTSController(),
+            builder: (controller) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        controller.testTTS("Hello", "en-US");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const CameraView(
+                                    lang: "ar",
+                                  )),
+                        );
+                      },
+                      child: Text('Arabic'),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const CameraView(
+                                    lang: "en-US",
+                                  )),
+                        );
+                      },
+                      child: Text('English'),
+                    ),
+                  ],
+                ),
+              );
+            }));
   }
 }
